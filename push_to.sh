6c92fdd9c5f134f03ff9aed8a33d1dd14ab22d58
s <Summeren@users.noreply.github.com>
2015-11-29 19:12:58 +0800

    push

--- push_to.sh
+++ push_to.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-page='../summerren.github.io/'
+page='../Summeren.github.io/'
 
-cp -rf output/* $page
+cp -rf ./output/* $page
 
 cd $page
 
 git add .
 git cm 'post'
 git push origin master
+
+cd -
